95cde939e4aa3f24f81612d212d44674a8365ed9
PawelBalawender <biomiteczq@gmail.com>
2018-07-01 12:39:23 +0200

    decreased TOLs from 1e-8 to 1e-6, cause it didnt round well. also added truncating numbers to integers in translate_to_python, cause ord() didnt want to display them

--- Polynomial.py
+++ Polynomial.py
@@ -392,7 +392,7 @@ def convert(source: str) -> str:
     return python
 
 if __name__ == '__main__':
-    run_tests()#
+    run_tests()
 
     # this program writes its input to std output; CAT program
     prog = 'f(x) = x^10 - 4827056x^9 + 1192223600x^8 - 8577438158x^7 + 958436165464x^6 - 4037071023854x^5 + 141614997956730x^4 - 365830453724082x^3 + 5225367261446055x^2 - 9213984708801250x + 21911510628393750'
